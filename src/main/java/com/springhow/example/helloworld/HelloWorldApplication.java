b37bfcf7054bccd2f5734a6609b20fbd8ad89742
NavnathChaudhari <98884491+NavnathChaudhari@users.noreply.github.com>
2022-09-21 16:21:35 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "this is navnath";
+        return "this is navnath hello";
     }
 
 }
